df63041d64cfdb91a51e2a10e597f1aba4d8b0e6
AgataHerbst <125795402+AgataHerbst@users.noreply.github.com>
2023-07-01 10:06:03 +0300

    откорректировала страницу navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-//import Logo from "../assets/weddingCake.png";
+import Logo from "../assets/weddingCake.png";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import ReorderIcon from '@mui/icons-material/Reorder';
@@ -12,6 +12,15 @@ setOpenLinks(!openLinks);
     };
 return (
     <div className="navbar">
+       <div className="leftSide" id={openLinks ? "open" : "close"}>
+        <img src={Logo} />
+        <div className="hiddenLinks">
+        <Link to="/"> Home </Link>
+        <Link to="/menu"> Galleries </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/contact"> Contact </Link>
+        </div>
+      </div>
      <div className="rightSide">
         <Link to="/"> Home </Link>
         <Link to="/galleries"> Galleries </Link>
